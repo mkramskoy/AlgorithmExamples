c00cd3766e7e3c87d1fccdedadd91fa77b481d0f
Michael Kramskoy <mkramskoy@gmail.com>
2016-09-02 01:17:43 +0300

    pew

---
+++ SCC_ios/SCC_ios/ViewController.swift
@@ -0,0 +1,247 @@
+//
+//  ViewController.swift
+//  SCC_ios
+//
+//  Created by Michael Kramskoy on 9/1/16.
+//  Copyright © 2016 wirrwarr. All rights reserved.
+//
+
+import UIKit
+
+//
+//  main.swift
+//  SCC
+//
+//  Created by Michael Kramskoy on 9/1/16.
+//  Copyright © 2016 wirrwarr. All rights reserved.
+//
+
+import Foundation
+
+public func ==(lhs: Vertex, rhs: Vertex) -> Bool {
+    
+    return lhs.key == rhs.key
+}
+
+public class Vertex: Hashable {
+    
+    let graph: SwiftGraph
+    
+    var key: Int
+    public var hashValue: Int {
+        return key
+    }
+    
+    var explored = false
+    var label: Int? {
+        didSet {
+            if let label = self.label {
+                self.graph.canvasByLabels[label] = self
+                //print("label set \(label) to vertex \(self.key)")
+            }
+        }
+    }
+    
+    var neighbors = Set<Vertex>()
+    
+    init (graph: SwiftGraph, key: Int) {
+        self.graph = graph
+        self.key = key
+    }
+    
+    func description() -> String {
+        
+        return "\(key)"
+    }
+}
+
+public class SwiftGraph {
+    
+    var canvas = Dictionary<Int, Vertex>()
+    var canvasByLabels = Dictionary<Int, Vertex>()
+    
+    public let isDirected: Bool = true
+    public var verticesCount: Int {
+        return canvas.keys.count
+    }
+    
+    func vertexForKey(key: Int) -> Vertex? {
+        return canvas[key]
+    }
+    
+    func vertexForLabel(label: Int) -> Vertex? {
+        return canvasByLabels[label]
+    }
+    
+    func getOrCreateVertexForKey(key: Int) -> Vertex {
+        
+        if let vertex = canvas[key] {
+            return vertex
+        }
+        else {
+            let vertex = Vertex(graph: self, key: key)
+            canvas[key] = vertex
+            
+            return vertex
+        }
+    }
+    
+    func addEdge(source source: Vertex, neighbor: Vertex) {
+        
+        if source.graph !== self || neighbor.graph !== self {
+            print("serious problem")
+        }
+        
+        source.neighbors.insert(neighbor)
+        
+        if isDirected == false {
+            neighbor.neighbors.insert(source)
+        }
+    }
+    
+    func edgeDescription() -> String {
+        
+        var desc = ""
+        for i in 0...self.verticesCount {
+            if let vertex = self.vertexForKey(i) {
+                let label = vertex.label ?? -1
+                desc += "key: \(vertex.key), label: \(label), edges: "
+                for neighbor in vertex.neighbors {
+                    desc+="\(neighbor.key) "
+                }
+                desc+="\n"
+            }
+        }
+        
+        return desc
+    }
+}
+
+class ViewController: UIViewController {
+
+    let graph = SwiftGraph()
+    let reversedGraph = SwiftGraph()
+    
+    var sccDict: Dictionary<Int, Int>?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var verticesArray = [String]()
+        
+        do {
+            let url: NSURL! = NSBundle.mainBundle().URLForResource("SCC", withExtension: "txt")
+            let string = try String(contentsOfURL: url, encoding: NSUTF8StringEncoding)
+            verticesArray = string.componentsSeparatedByString("\n")
+            verticesArray.removeLast()
+        }
+        catch {
+            print("")
+        }
+        
+        for edgesString in verticesArray {
+            var edgesArray = edgesString.componentsSeparatedByString(" ")
+            edgesArray.removeLast()
+            
+            var integers = edgesArray.map { Int($0)! }
+            
+            let sourceVertex = graph.getOrCreateVertexForKey(integers.removeFirst())
+            
+            let rNeighborVertex = reversedGraph.getOrCreateVertexForKey(sourceVertex.key)
+            
+            for neighborKey in integers {
+                let neighborVertex = graph.getOrCreateVertexForKey(neighborKey)
+                graph.addEdge(source: sourceVertex, neighbor: neighborVertex)
+                
+                let rSourceVertex = reversedGraph.getOrCreateVertexForKey(neighborKey)
+                reversedGraph.addEdge(source: rSourceVertex, neighbor: rNeighborVertex)
+            }
+        }
+        
+        print("verticesCount = \(graph.verticesCount)")
+        //print("Normal graph:\n\(graph.edgeDescription())")
+
+        self.firstDFSLoop(reversedGraph)
+        //print("Reversed graph:\n\(reversedGraph.edgeDescription())")
+        
+        self.sccDict = Dictionary<Int, Int>()
+        
+        self.secondDFSLoop(graph, reversedGraph: reversedGraph)
+        
+        //print("SCC dict \(self.sccDict)")
+        
+    }
+    
+    func firstDFSLoop(graph: SwiftGraph) {
+        
+        var label = 0
+        
+        for i in graph.verticesCount.stride(to: 0, by: -1) {
+            if let vertex = graph.vertexForKey(i) {
+                if vertex.explored == false {
+                    self.DFS(vertex, label: &label)
+                }
+            }
+            else {
+                print("serious error happened")
+            }
+        }
+    }
+    
+    func secondDFSLoop(graph: SwiftGraph, reversedGraph: SwiftGraph) {
+        
+        var label = 0
+        
+        for i in graph.verticesCount.stride(to: 0, by: -1) {
+            if let vertexFromReversedGraph = reversedGraph.vertexForLabel(i),
+                let vertex = graph.vertexForKey(vertexFromReversedGraph.key) {
+                
+                if vertex.explored == false {
+                    //print("secondLoop vertex \(vertex.description())")
+                    
+                    let labelBeforeDFS = label
+                    
+                    self.DFS(vertex, label: &label)
+                    
+                    let componentSize = label - labelBeforeDFS
+                    if self.sccDict != nil {
+                        self.sccDict![vertex.key] = componentSize
+                    }
+                }
+            }
+            else {
+                print("serious error happened")
+            }
+        }
+    }
+    
+    func DFS(vertex: Vertex, inout label: Int) {
+        
+        vertex.explored = true
+        
+        for neighbor in vertex.neighbors {
+            if neighbor.explored == false {
+                self.DFS(neighbor, label: &label)
+            }
+        }
+        
+        label += 1
+        vertex.label = label
+    }
+    
+    struct IntStack {
+        
+        var items = [Int]()
+        
+        mutating func push(item: Int) {
+            
+            items.append(item)
+        }
+        
+        mutating func pop() -> Int {
+            
+            return items.removeLast()
+        }
+    }
+}
+
